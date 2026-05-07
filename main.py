4fa6cecfd9c957ca678e31e8d67966ebf991a92d
nardos7823 <nardosgetachewnardo@gmail.com>
2026-05-07 12:08:11 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+I created a real-life application where an individual's body mass index is measured to assess one's health. Being new to programming and having acquired my Python certification, I sought to utilize what I have learned about using operators and conditional statements within the language.
+
+For instance, the software will first take a user's weight in kilograms and height in meters as input. The body mass index formula will be applied, after which 'if-elif-else' clauses will be used to determine whether the calculated number indicates 'Underweight', 'Normal', or 'Overweight'. In the process of working on this project, my efforts were channeled towards developing a user-friendly interface and doing the calculations properly.
+
+To test the applicability of my creation, I chose to use my own values. For example, based on the fact that I weigh 63kg and am 1.65m tall, my Body Mass Index is 23.14, meaning I fall under the 'Normal Weight' bracket.
